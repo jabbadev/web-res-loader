f755d40ab8d376f5ebb96658eca47e38d942cc68
Francesco Dalprà <francesco.dalpra@ubiss.it>
2011-11-24 09:53:35 +0100

    Integrazione con jQuery

--- js/resloader.js
+++ js/resloader.js
@@ -1,26 +1,48 @@
 (function(global,obj){
 	
 	function resloader() {
-		this.jsList = null;
-		this.cssList = null;
-		this.htmlList = null;
-		
-		this.loadConfig = function(resconf,initCallBack,getExec){
-			this.jsList = resconf.jsList;
-			this.cssList = resconf.cssList;
-			this.htmlList = resconf.htmlList;
-			( initCallBack ) && initCallBack();
-			if ( getExec ){
-				this.setUpGetExec(getExec);
-			}
+
+		this.loadConfig = function(jq,rlcURL,callback){
+			var self = this;
+			this.jq = jq;
+			this.rlcURL = rlcURL;
+			this._loadJS(this.rlcURL,
+			function(){
+				self.setConfig();
+				callback();
+			});
 		};
 		
-		this.setUpGetExec = function(getExec){
-			this.getExec = function(url,ctxt){getExec.call(ctxt,url);};
-		};
+		this.setConfig = function(){
+			this.jsList = global.rlconfig.jsList;
+			this.cssList = global.rlconfig.cssList;
+			this.htmlList = global.rlconfig.htmlList;
+		}
 		
-		this.loadHTML = function(htmlName,ctxt) {
-			this.getExec(this.htmlList[htmlName].url,ctxt);
+		this._loadJS = function(jsURL,handler){
+			var plugin_script_tag = document.createElement("script");
+			plugin_script_tag.setAttribute("type","text/javascript");
+			plugin_script_tag.setAttribute("charset","utf-8");
+			plugin_script_tag.setAttribute("src", jsURL);
+			plugin_script_tag.onload = handler;
+			plugin_script_tag.onreadystatechange = function () { /* Same thing but for IE */
+				if (this.readyState == "complete" || this.readyState == "loaded") {
+					handler();
+			    }
+			};
+			(document.getElementsByTagName("head")[0] || document.documentElement).appendChild(plugin_script_tag);
+		};
+			
+		this.loadHTML = function(htmlName,success,error,ctxt,async) {
+			var proxySuccess = this.jq.proxy(success,ctxt);
+			var proxyError = this.jq.proxy(error,ctxt);
+			
+			this.jq.ajax({
+				async : ( async === undefined && true ) || async,
+				url : this.htmlList[htmlName].url,
+				success : proxySuccess,
+				error : proxyError
+			});
 		};
 		
 		this.waitForDep = function( js ) {
@@ -93,21 +115,18 @@
 			}
 		};
 		
-		this.docguiInitChain = function() {
-			this.loadCSS("uiDefaulTema");
-			this.docgui.loadJS("baseWidget");
-		};
-		
 		this.loadCssList = function( cssList ){
 			for ( var i in cssList ){
 				this.loadCSS(cssList[i]);
 			}
 		};
 		
-		this.loadJsList = function( widgetList ){
+		this.loadJsList = function(widgetList,callback,ctxt){
 			for ( var i in widgetList ){
 				this.loadJS(widgetList[i]);
 			}
+			var proxyCallback = this.jq.proxy(callback,ctxt);
+			this.jsAreReady(widgetList,proxyCallback);
 		};
 		
 		this.jsAreReady = function ( jsList, callback ) {
